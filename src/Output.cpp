e1bb90205a581c56ccab1c0284bfa5f8c2e375a3
Elviss Strazdins <elviss@elviss.lv>
2016-05-11 03:03:19 +0300

    Print debug message when reading outside of the input buffer

--- src/Output.cpp
+++ src/Output.cpp
@@ -233,7 +233,12 @@ void Output::handleRead(const std::vector<uint8_t>& newData)
             }
         }
     }
-    
+
+    if (offset > data.size())
+    {
+        std::cout << "Reading outside of the buffer" << std::endl;
+    }
+
     data.erase(data.begin(), data.begin() + offset);
 
 #ifdef DEBUG
