2975ee2f9b2c52193bfc5f4f68388e2db456e648
Elviss Strazdins <elviss@elviss.lv>
2016-07-26 12:01:47 +0300

    Remove unneeded "Input" from log

--- src/Receiver.cpp
+++ src/Receiver.cpp
@@ -237,7 +237,7 @@ namespace relay
 
         reset();
 
-        std::cout << "[" << name << "] " << "Input disconnected" << std::endl;
+        std::cout << "[" << name << "] " << "Disconnected" << std::endl;
     }
 
     bool Receiver::handlePacket(const rtmp::Packet& packet)
