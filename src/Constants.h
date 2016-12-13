165a15b6ef2044bd53299e0f8834e003b8ff429b
Elviss Strazdins <elviss@elviss.lv>
2016-12-13 17:34:18 +0200

    Reduce RTMP client and server version to avoid handshake signature

--- src/Constants.h
+++ src/Constants.h
@@ -9,5 +9,5 @@
 static const uint8_t RTMP_RELAY_VERSION[2] = { 0, 4 };
 
 static const uint8_t RTMP_VERSION = 3;
-static const uint8_t RTMP_SERVER_VERSION[4] = { 9, 0, 124, 2 };
-static const uint8_t RTMP_CLIENT_VERSION[4] = { 9, 0, 124, 2 }; // { 0x0C, 0x0O, 0x0D, 0x0E };
+static const uint8_t RTMP_SERVER_VERSION[4] = { 2, 0, 0, 0 };
+static const uint8_t RTMP_CLIENT_VERSION[4] = { 2, 0, 0, 0 }; // { 0x0C, 0x0O, 0x0D, 0x0E };
