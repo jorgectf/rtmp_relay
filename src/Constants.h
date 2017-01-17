923d229b9fcd306b1476b8621703f1f69ab0c9f5
Elviss Strazdins <elviss@elviss.lv>
2017-01-17 11:16:34 +0200

    Increase version number to 0.7

--- src/Constants.h
+++ src/Constants.h
@@ -6,8 +6,8 @@
 
 #include <cstdint>
 
-static const uint8_t RTMP_RELAY_VERSION[2] = { 0, 6 };
+static const uint8_t RTMP_RELAY_VERSION[2] = {0, 7};
 
 static const uint8_t RTMP_VERSION = 3;
-static const uint8_t RTMP_SERVER_VERSION[4] = { 2, 0, 0, 0 };
-static const uint8_t RTMP_CLIENT_VERSION[4] = { 2, 0, 0, 0 }; // { 0x0C, 0x0O, 0x0D, 0x0E };
+static const uint8_t RTMP_SERVER_VERSION[4] = {2, 0, 0, 0};
+static const uint8_t RTMP_CLIENT_VERSION[4] = {2, 0, 0, 0}; // {0x0C, 0x0O, 0x0D, 0x0E};
